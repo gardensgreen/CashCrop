180ff28b8075a9b1676ace0b702f4acd73220c66
Daniel TIllero <danieltillero67@gmail.com>
2021-03-22 10:39:46 -0400

    Changed App.js root component to Root.js

---
+++ frontend/src/Root.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Root() {
+    return <h1>Hello world!</h1>;
+}
+
+export default Root;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Root from "./Root";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
